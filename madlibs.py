07427aa79441b34d179e03171c0739242f263448
Aditya Danturthi <aditya2096@gmail.com>
2022-02-20 22:42:54 -0330

    Add files via upload
    This file contains different ways of string concatenation with a pre-defined variable and also using user input.

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+#String concatenation
+youtuber="Aditya Danturthi" #string variable
+
+#Different ways of string concatenation
+print("Subscribe to "+youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
+
+adj=input("Adjective: ")
+verb1=input("Verb: ")
+verb2=input("Verb: ")
+famous_person=input("Famous person: ")
+madlib= f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}, stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)

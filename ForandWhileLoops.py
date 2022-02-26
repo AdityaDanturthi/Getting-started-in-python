ffc4b286a30ab561f4f5865bedb0a2d8a137ec58
Aditya Danturthi <aditya2096@gmail.com>
2022-02-25 22:36:44 -0330

    Add files via upload
    'For' and 'While' loop implementation: 
    
    While loop example: Printing the statement "Hello World!" while the condition is true and decrementing the loop variable
    
    For loop: Iterating over the variable 'months' to print all the months

---
+++ ForandWhileLoops.py
@@ -0,0 +1,10 @@
+x = 0
+#while loop
+while(x>0):
+    print('Hello World!')
+    x=x-1
+
+
+months= ['Janurary','February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
+for x in months:
+    print(x)

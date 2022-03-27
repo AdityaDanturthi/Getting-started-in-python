020ddc7273f12407b6a0e3f1bff42bda10c21ffd
Aditya Danturthi <aditya2096@gmail.com>
2022-03-27 20:49:18 -0230

    Permutations using nested loops

---
+++ ListComprehensions.py
@@ -0,0 +1,12 @@
+x=2;
+y=2;
+z=2;
+n=2;
+list=[];
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if(i+j+k) != n:
+                list.append([i,j,k]);
+
+print(list);
